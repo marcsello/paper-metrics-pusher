b6e20e82d2b36287d6826d91d0ff6c99c424f908
marcsello <punkosdmarcell@rocketmail.com>
2020-06-11 18:58:44 +0200

    fixed typo

--- src/main/java/com/marcsello/metricspusher/MetricsPusher.java
+++ src/main/java/com/marcsello/metricspusher/MetricsPusher.java
@@ -1,8 +1,8 @@
-package com.marcsello.meticspusher;
+package com.marcsello.metricspusher;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class MeticsPusher extends JavaPlugin {
+public final class MetricsPusher extends JavaPlugin {
 
     @Override
     public void onEnable() {
